17607e847e0c003a1a48d7360114a2f9ae7b0800
B23579 <bauricenafack@yahoo.fr>
2022-06-10 09:52:34 +0200

    stream

--- transpose.cpp
+++ transpose.cpp
@@ -5,11 +5,12 @@
 
 int main()
 {
-	int const n=500;
+	int  n=600;
 	int N=100;
+	for(int n:{150,200,250,300,350,400,450,500,550,600,700}){
 	for(int m =0 ; m<N;m++){
-		double A[n][n];
-		double B[n][n];
+		 double A[n][n];
+		 double B[n][n];
 	
 		for ( int i=0;i<n; i++){
 				for(int j=0;j<n;j++){
@@ -24,12 +25,12 @@ int main()
 				}}
 		
 	auto end = std::chrono::high_resolution_clock::now();
-	 B[1][1] = B[1][1]+;
-	auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end-start).count(); //microseconds
-	std::cout<< " Time in  millisecond   " << duration<<std::endl;
+	 B[1][1] = B[1][1]+1;
+	auto duration = std::chrono::duration_cast<std::chrono::nanoseconds>(end-start).count(); //microseconds
+	std::cout<<n<<";"<< duration<<std::endl;
 	}
 
-
+	}
 	return 0;
 
 }
